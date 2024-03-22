58c78601d2237c1e79cd1ac075fa01d6227ac482
lamarcusmarx923 <161892041+lamarcusmarx923@users.noreply.github.com>
2024-03-22 15:09:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ const $items = document.querySelectorAll('.menu--item')
 let menuHeight = $menu.clientHeight
 let itemHeight = $items[0].clientHeight
 let wrapHeight = $items.length * itemHeight
+
+let scrollSpeed = 0
+let oldScrollY = 0
+let scrollY = 0
+let y = 0
